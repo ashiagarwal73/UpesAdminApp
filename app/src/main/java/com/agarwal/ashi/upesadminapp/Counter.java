b1f9217e8c09e3aad10640cb7f08e81150cae058
Ashi <500052000@stu.upes.ac.in>
2018-03-15 14:15:49 +0530

    added counter

---
+++ app/src/main/java/com/agarwal/ashi/upesadminapp/Counter.java
@@ -0,0 +1,17 @@
+package com.agarwal.ashi.upesadminapp;
+
+/**
+ * Created by Ashi on 15-03-2018.
+ */
+
+public class Counter {
+    public int getCounterid() {
+        return counterid;
+    }
+
+    public void setCounterid(int counterid) {
+        this.counterid = counterid;
+    }
+
+    int counterid;
+}
