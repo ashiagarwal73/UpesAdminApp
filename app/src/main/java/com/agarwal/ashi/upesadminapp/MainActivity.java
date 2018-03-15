9ec848b33f97a40be7435e0e4b9785c0ef3821b7
Ashi <500052000@stu.upes.ac.in>
2018-03-15 22:07:39 +0530

    sending data working with database

--- app/src/main/java/com/agarwal/ashi/upesadminapp/MainActivity.java
+++ app/src/main/java/com/agarwal/ashi/upesadminapp/MainActivity.java
@@ -1,41 +1,184 @@
 package com.agarwal.ashi.upesadminapp;
 
-import android.os.Build;
-import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
     String sschool,sworkshop,sseminar,scompetition,scultural,ssports,swebinar;
-//  yes
     int count=0;
     Counter counter;
+    String society[]={"choose society"};
+    EditText eventname,eventDesc,date,organiser,contact;
+    RadioGroup workshop,seminar,competition,cultural,sports,webinar;
+    Spinner schoolspinner,societyspinner;
     Button submit;
-     DatabaseReference mDatabase;
+    String selectedsociety;
+    String seventdesc;
+    String sdate;
+    String sorganiser;
+    String scontact;
+    String selectedSchool="Choose School";
+    String schools[]={"Choose School","School of Computer Science","School of Engineering","School of Design","School of Business","School of Law"};
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eventname=(EditText)findViewById(R.id.eventname);
+        eventDesc=(EditText)findViewById(R.id.eventDescription);
+        date=(EditText)findViewById(R.id.date);
+        organiser=(EditText)findViewById(R.id.organiser);
+        contact=(EditText)findViewById(R.id.contact);
+        workshop=findViewById(R.id.workshop);
+         seminar=findViewById(R.id.seminar);
+         competition=findViewById(R.id.competition);
+         cultural=findViewById(R.id.cultural);
+        sports=findViewById(R.id.sports);
+         webinar=findViewById(R.id.webminar);
+        final ArrayList<Society> societyArrayList= new ArrayList<Society>();
+        ArrayAdapter<String> aA=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,schools);
         mDatabase= FirebaseDatabase.getInstance().getReference();
+        schoolspinner=findViewById(R.id.schoolspinner);
+        societyspinner=findViewById(R.id.societyspinner);
+        schoolspinner.setAdapter(aA);
+        schoolspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               selectedSchool=parent.getItemAtPosition(position).toString();
+               if(selectedSchool.equals("Choose School"))
+               {
+                   societyspinner.setVisibility(View.INVISIBLE);
+               }
+               else {
+                   societyspinner.setVisibility(View.VISIBLE);
+                   mDatabase.addValueEventListener(new ValueEventListener() {
+                       @Override
+                       public void onDataChange(DataSnapshot dataSnapshot) {
+                           for (DataSnapshot q :dataSnapshot.child("Society").getChildren())
+                                {
+                                    if(q.getValue(Society.class).getSchool()!=null)
+                                    {
+                                        if(q.getValue(Society.class).getSchool().equals(selectedSchool))
+                                        {
+                                            societyArrayList.add(q.getValue(Society.class));
+                                        }
+
+                                    }
+                                }
+                                society=new String[societyArrayList.size()];
+                                for (int i=0;i<societyArrayList.size();i++)
+                                {
+                                    society[i]=societyArrayList.get(i).getSocietyName();
+                                }
+                                societyArrayList.clear();
+                           ArrayAdapter<String> aB=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,society);
+                           societyspinner.setAdapter(aB);
+                           societyspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                               @Override
+                               public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                   selectedsociety=parent.getItemAtPosition(position).toString();
+                               }
+
+                               @Override
+                               public void onNothingSelected(AdapterView<?> parent) {
+
+                               }
+                           });
+
+                       }
+
+                       @Override
+                       public void onCancelled(DatabaseError databaseError) {
+
+                       }
+                   });
+               }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         submit=findViewById(R.id.submit);
+        workshop.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                sworkshop=radioButton.getText().toString();
+
+            }
+        });
+        seminar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                sseminar=radioButton.getText().toString();
+
+            }
+        });
+        competition.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                scompetition=radioButton.getText().toString();
+
+            }
+
+
+        });
+        sports.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                ssports=radioButton.getText().toString();
+
+            }
+
+
+        });
+        cultural.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                scultural=radioButton.getText().toString();
+
+            }
+
+
+        });
+        webinar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton=findViewById(checkedId);
+                swebinar=radioButton.getText().toString();
+
+            }
+
+
+        });
         submit.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
+
                 mDatabase.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -49,116 +192,27 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                 });
-                EditText eventname=(EditText)findViewById(R.id.eventname);
-                EditText eventDesc=(EditText)findViewById(R.id.eventDescription);
-                EditText date=(EditText)findViewById(R.id.date);
-                EditText organiser=(EditText)findViewById(R.id.organiser);
-                EditText contact=(EditText)findViewById(R.id.contact);
-                String seventname=null;
-                String seventdesc;
-                String sdate;
-                String sorganiser;
-                String scontact;
-
-                seventname=eventname.getText().toString();
-                seventdesc=eventDesc.getText().toString();
-                sdate=date.getText().toString();
-                sorganiser=organiser.getText().toString();
-                scontact=contact.getText().toString();
-
-                RadioGroup school=findViewById(R.id.school);
-                RadioGroup workshop=findViewById(R.id.workshop);
-                RadioGroup seminar=findViewById(R.id.seminar);
-                RadioGroup competition=findViewById(R.id.competition);
-                RadioGroup cultural=findViewById(R.id.cultural);
-                RadioGroup sports=findViewById(R.id.sports);
-                RadioGroup webinar=findViewById(R.id.webminar);
-
-//                school.setOnContextClickListener(new RadioGroup.OnCheckedChangeListener(){
-//                    @Override
-//                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                        RadioButton radioButton=findViewById(checkedId);
-//                        sschool=radioButton.getText().toString();
-//
-//                    }
-//
-//
-//                });
-
-                workshop.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        sworkshop=radioButton.getText().toString();
-
-                    }
-
-
-                });
-
-                seminar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        sseminar=radioButton.getText().toString();
-
-                    }
-
-
-                });
-
-                competition.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        scompetition=radioButton.getText().toString();
-
-                    }
-
-
-                });
-
-                cultural.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        scultural=radioButton.getText().toString();
-
-                    }
-
-
-                });
-
-                sports.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        ssports=radioButton.getText().toString();
-
-                    }
-
-
-                });
-
-                webinar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        RadioButton radioButton=findViewById(checkedId);
-                        swebinar=radioButton.getText().toString();
-
-                    }
-
-
-                });
                 EventsInformation eventsInformation = new EventsInformation();
                 Counter counter=new Counter();
-                eventsInformation.setEventName("new event");
-               counter.setCounterid(++count);
+                counter.setCounterid(++count);
                 mDatabase.child("Counter").setValue(counter);
+                eventsInformation.setEventName(eventname.getText().toString());
+                eventsInformation.setEventDescription(eventDesc.getText().toString());
+                sdate=date.getText().toString();
+                eventsInformation.setDate(sdate);
+                sorganiser=organiser.getText().toString();
+                eventsInformation.setOrganiser(sorganiser);
+                scontact=contact.getText().toString();
+                eventsInformation.setContact(scontact);
+                eventsInformation.setWorkshop(sworkshop);
+                eventsInformation.setSeminar(sseminar);
+                eventsInformation.setCompetition(scompetition);
+                eventsInformation.setCultural(scultural);
+                eventsInformation.setSports(ssports);
+                eventsInformation.setWebminar(swebinar);
+                eventsInformation.setSchool(selectedSchool);
                 mDatabase.child("EventsDetails").child(counter.getCounterid() + "").setValue(eventsInformation);
             }
         });
-
-        //Toast.makeText(SplashActivity.this, ""+counter.getCounterid(), Toast.LENGTH_SHORT).show();
     }
 }
